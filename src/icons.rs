1a50b193fe559f86ec11659a58e71ecd7dfcb452
Jason Nader <jason.nader@protonmail.com>
2021-11-06 18:21:18 +0900

    music block: pango escape default text icons

--- src/icons.rs
+++ src/icons.rs
@@ -57,8 +57,10 @@ impl Default for Icons {
             "music" => "MUSIC",
             "music_next" => ">",
             "music_pause" => "||",
-            "music_play" => ">",
-            "music_prev" => "<",
+            // these need to be pango escaped
+            // TODO: more general fix?
+            "music_play" => "&gt;",
+            "music_prev" => "&lt;",
             "net_down" => "DOWN",
             "net_loopback" => "LO",
             "net_up" => "UP ",
